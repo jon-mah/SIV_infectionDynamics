c6578ad428ed08360bd3bd9e66c6886ffa29dbb6
Jonathan Mah <jonmah@uw.edu>
2018-07-27 08:23:54 -0700

    Fixed images for b/w printing

--- Scripts/exampleModel.m
+++ Scripts/exampleModel.m
@@ -4,12 +4,12 @@ clc;
 options = odeset('RelTol', 1e-4, 'NonNegative', [1 2 3]);
 figure
 [t, x] = ode45('siv', [0 3650], [100 300 600], options);
-plot(t, x);
+plot(t, x(:,1), 'b:', t, x(:,2), 'r-.', t, x(:,3), 'g--')
 legend('S', 'I', 'V')
 title('SIV model')
 
 figure
 [t, x] = ode45('sir', [0, 10], [999, 1, 0], options);
-plot(t, x);
+plot(t,x(:,1),'g',t,x(:,2),'b--o',t,x(:,3),'c*')
 legend('S', 'I', 'R')
 title('SIR model')
